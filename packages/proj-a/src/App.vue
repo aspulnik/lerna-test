4aff7e0b2f01f5aad3c6180b3d00d1eca8ccaeec
Artur Spulnik <aspulnik@tbscg.com>
2020-12-02 09:50:48 +0100

    breaking change: app1

--- packages/proj-a/src/App.vue
+++ packages/proj-a/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header title="Test" />
-    Bobcat<ksimg alt="Vue logo" style="height:300px" src="https://logos-download.com/wp-content/uploads/2018/04/Bobcat_logo_black_cercle-700x700.png">
+    Bobcadt<ksimg alt="Vue logo" style="height:300px" src="https://logos-download.com/wp-content/uploads/2018/04/Bobcat_logo_black_cercle-700x700.png">
     <Footer title="Test Footer" />
   </div>
 </template>
