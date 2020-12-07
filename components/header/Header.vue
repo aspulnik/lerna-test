5f486f5bd4df4c2acc4a733c192089d6d9687e72
Artur Spulnik <aspulnik@tbscg.com>
2020-12-07 12:38:43 +0100

    fix: app2

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -2,7 +2,7 @@
  <header>
     <h1>{{ title }}</h1>
     <p>Welcome1dd11</p>
-    <p>Some addfitional informat1iodn here</p>
+    <p>Some addfitional informat1iod1n here</p>
   </header>
 </template>
 
